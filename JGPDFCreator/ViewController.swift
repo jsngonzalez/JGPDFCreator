f9850e005b6ddce5f0108a29d5ec614dade98573
Jeisson Gonzalez <jsn8802@gmail.com>
2017-12-14 10:01:40 -0500

    Initial Commit

---
+++ JGPDFCreator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JGPDFCreator
+//
+//  Created by Jeisson González on 14/12/17.
+//  Copyright © 2017 wigilabs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
